3603085dde416a57fa4c1a86b9035d78f7867488
Ilya Basiuk <ilyabasiuk@gmail.com>
2016-01-18 18:02:44 +0300

    l6 module.parent

---
+++ start/app.js
@@ -0,0 +1,4 @@
+var server = require("./server");
+
+console.log("app started");
+server.run();
--- start/server.js
+++ start/server.js
@@ -1,7 +1,15 @@
 var user = require("./user");
 
-var vasya = new user.User("Вася");
-var petya = new user.User("Петя");
+function run() {
+	var vasya = new user.User("Вася");
+	var petya = new user.User("Петя");
 
-vasya.hello(petya);
-console.log(global.test);
+	vasya.hello(petya);
+	console.log(global.test);
+}
+
+if (module.parent) {
+	exports.run = run;
+} else {
+	run();
+}
