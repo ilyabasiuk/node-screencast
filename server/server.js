115d7dfbead39c0ee512e9efccd4d1ac21a15b64
Ilya Basiuk <ilyabasiuk@gmail.com>
2016-01-20 18:50:05 +0300

    l14 added loging all server events

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 var http = require("http");
 var server = new http.Server(); // EventEmitter
-
+// hrrp.Server -> net.Server -> EventEmitter
 server.listen(1377, "127.0.0.1");
+var counter = 0;
+console.log(http.Server);
+var emit = server.emit;
 
+server.emit = function (name) {
+	console.log(name);
+	emit.apply(server, arguments);
+};
 server.on("request", function (req, res) {
-	res.end("Hello World!!");
-})
+	res.end("Hello World!!" + ++counter);
+});
