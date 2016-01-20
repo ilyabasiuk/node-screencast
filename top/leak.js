37700b179268755a3d481baa5f20f620a9fc4cd4
Ilya Basiuk <ilyabasiuk@gmail.com>
2016-01-20 16:46:21 +0300

    l13 leaks

--- top/leak.js
+++ top/leak.js
@@ -1,6 +1,6 @@
 var EventEmitter = require("events").EventEmitter;
 var db = new EventEmitter;
-
+//db.setMaxListeners(0);
 
 function Request() {
 	var self = this;
@@ -9,18 +9,24 @@ function Request() {
 
 	this.send = function (data) {
 		console.log(data);
-	}
+	};
 
 	this.onError = function () {
 		self.send("Huston, we have a problem");
-	}
-
-	db.on("data", function (info) {
+	};
+	function onData(info) {
 		self.send(info);
-	})
+	}
+	this.end = function () {
+		db.removeListener("data", onData);
+	};
+
+	db.on("data", onData);
 };
 
 setInterval(function () {
+	// heap dump
 	var request = new Request();
+	//request.end();
 	console.log(process.memoryUsage().heapUsed, db.listenerCount("data"));
 }, 200)
