6d6b09e07f01466e507884853e48ef6d5bae74a3
Ilya Basiuk <ilyabasiuk@gmail.com>
2016-01-21 11:17:12 +0300

    l17 supervisor

--- server/echo.js
+++ server/echo.js
@@ -4,15 +4,15 @@ var url = require("url");
 
 var server = new http.Server(function (req, res) {
 	var urlParsed = url.parse(req.url, true);
-	console.log(req.headers);
 	if (urlParsed.pathname === "/echo" && urlParsed.query.message) {
 		res.setHeader("cache-control", "no-cache"); //removeHeader
 		//res.writeHead(200, "OK", {"Cache-control": "no-cache"});
 		res.end(urlParsed.query.message);
-	} else {
-		res.statusCode = 404;
-		res.end("Page not found");
+		return
 	}
+
+	res.statusCode = 404;
+	res.end("Page not found");
 });
 
 server.listen(1377, "127.0.0.1");
---
+++ server/newfile.js
@@ -0,0 +1,5 @@
+// changing new file will not influence restarting server by supervisor
+
+// just have checked it and seems that now it's is supported
+
+// and make sure that you are not watching on node_modules folders
