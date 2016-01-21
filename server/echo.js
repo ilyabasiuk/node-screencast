063cb5b152245aeba50c9bc5da98c0557836c451
Ilya Basiuk <ilyabasiuk@gmail.com>
2016-01-21 10:49:04 +0300

    l15 headers

--- server/echo.js
+++ server/echo.js
@@ -4,7 +4,10 @@ var url = require("url");
 
 var server = new http.Server(function (req, res) {
 	var urlParsed = url.parse(req.url, true);
+	console.log(req.headers);
 	if (urlParsed.pathname === "/echo" && urlParsed.query.message) {
+		res.setHeader("cache-control", "no-cache"); //removeHeader
+		//res.writeHead(200, "OK", {"Cache-control": "no-cache"});
 		res.end(urlParsed.query.message);
 	} else {
 		res.statusCode = 404;
