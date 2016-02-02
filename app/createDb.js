3deb3a283d5e17dbb1146e3ccab6f3335428a8aa
Ilya Basiuk <ilyabasiuk@gmail.com>
2016-02-02 16:28:54 +0300

    app6 async series

--- app/createDb.js
+++ app/createDb.js
@@ -6,36 +6,49 @@ var User = require("models/user").User;
 // 1. drop db
 // 2. create & save 3 users
 // 3. close connection
-console.log(mongoose.connection.readyState);
 
-mongoose.connection.on("open", function () {
-	var db = mongoose.connection.db;
-	db.dropDatabase(function (err) {
-		if (err) throw err;
-
-		async.parallel([
-			function (callback) {
-				var vasya = new User({username: "Вася", password: "supervasya"});
-				vasya.save(function (err) {
-					callback(err, vasya);
-				});
-			},
-			function (callback) {
-				var petya = new User({username: "Петя", password: "123"});
-				petya.save(function (err) {
-					callback(err, petya);
-				});
-			},
-			function (callback) {
-				var admin = new User({username: "Петя", password: "thetruehero"});
-				admin.save(function (err) {
-					callback(err, admin);
-				});
-			}
-		], function (err, results) {
-			console.log(arguments);
-			mongoose.disconnect();
-		});
-
-	});
+async.series([
+	open,
+	dropDatabase,
+	createUsers,
+	close
+], function (err, results) {
+	if (err) throw err;
+	console.log(arguments);
 });
+
+function open(callback) {
+	mongoose.connection.on("open", callback);
+}
+
+function dropDatabase (callback) {
+	var db = mongoose.connection.db;
+	db.dropDatabase(callback);
+}
+
+function createUsers (callback) {
+	async.parallel([
+		function (callback) {
+			var vasya = new User({username: "Вася", password: "supervasya"});
+			vasya.save(function (err) {
+				callback(err, vasya);
+			});
+		},
+		function (callback) {
+			var petya = new User({username: "Петя", password: "123"});
+			petya.save(function (err) {
+				callback(err, petya);
+			});
+		},
+		function (callback) {
+			var admin = new User({username: "фвьшт", password: "thetruehero"});
+			admin.save(function (err) {
+				callback(err, admin);
+			});
+		}
+	], callback);
+}
+
+function close(callback) {
+	mongoose.disconnect(callback);
+}
