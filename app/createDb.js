b99df46d8ac5543b79ed7e31cab621b9bbb22c1e
Ilya Basiuk <ilyabasiuk@gmail.com>
2016-02-02 16:20:39 +0300

    app6 creating test db, using async

--- app/createDb.js
+++ app/createDb.js
@@ -1,15 +1,41 @@
-require("libs/log");
+var mongoose = require("libs/mongoose");
+var async = require("async"); // Promise, Fibers
 var User = require("models/user").User;
 
-var user = new User({
-	username: "Tester2",
-	password: "secret"
-});
 
-user.save(function (err, user, affected) {
-	if (err) throw err;
-	console.log(arguments);
-	User.findOne({username: "Tester"}, function (err, tester) {
-		console.log(tester);
-	})
+// 1. drop db
+// 2. create & save 3 users
+// 3. close connection
+console.log(mongoose.connection.readyState);
+
+mongoose.connection.on("open", function () {
+	var db = mongoose.connection.db;
+	db.dropDatabase(function (err) {
+		if (err) throw err;
+
+		async.parallel([
+			function (callback) {
+				var vasya = new User({username: "Вася", password: "supervasya"});
+				vasya.save(function (err) {
+					callback(err, vasya);
+				});
+			},
+			function (callback) {
+				var petya = new User({username: "Петя", password: "123"});
+				petya.save(function (err) {
+					callback(err, petya);
+				});
+			},
+			function (callback) {
+				var admin = new User({username: "Петя", password: "thetruehero"});
+				admin.save(function (err) {
+					callback(err, admin);
+				});
+			}
+		], function (err, results) {
+			console.log(arguments);
+			mongoose.disconnect();
+		});
+
+	});
 });
