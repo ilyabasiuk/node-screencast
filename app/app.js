34cb544ea1dcc61bf798491d8ba8005944479e67
Ilya Basiuk <ilyabasiuk@gmail.com>
2016-02-05 17:44:53 +0300

    app8 sessions

--- app/app.js
+++ app/app.js
@@ -4,10 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 var log = require("libs/log")(module);
 var error = require("error");
 var HttpError = error.HttpError;
+var mongoose = require("libs/mongoose");
 //var routes = require('./routes/index');
 var routes = require("./routes/users");
 
@@ -27,10 +28,27 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+const MongoStore = require('connect-mongo')(session);
+
+app.use(session({ resave: true,
+					saveUninitialized: true,
+					cookie: config.get("session:cookie"),
+					name: config.get("session:key"),
+					secret: config.get("session:secret"),
+					store: new MongoStore({
+						mongooseConnection: mongoose.connection
+					}) }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+	req.session.numberOfVisits =  req.session.numberOfVisits + 1 || 1;
+	//res.send("Visits:" + req.session.numberOfVisits);
+	next();
+});
+
 app.use(require("middleware/sendHttpError"));
-app.use('/', routes);
+app.use("/", routes);
 
 app.use(function (err, req, res, next) {
 	//NODE_ENV = "production"
